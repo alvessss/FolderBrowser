28642f5f2db7eec1b4128154ea553d121cf089de
Alvessss <backtobluepill@gmail.com>
2022-05-03 00:21:57 -0300

    finished building settings

--- FolderBrowser/src/main/java/com/alvessss/folderbrowser/FileSupport.java
+++ FolderBrowser/src/main/java/com/alvessss/folderbrowser/FileSupport.java
@@ -3,12 +3,32 @@ package com.alvessss.folderbrowser;
 @SuppressWarnings("all")
 public class FileSupport
 {
+   private String name;
    private final int fileIconId;
    private final String[] supportedExtensions;
 
-   public FileSupport(int fileIconId, String... supportedExtensions)
+   public FileSupport(
+      String name,
+      int fileIconId,
+      String... supportedExtensions)
    {
+      this.name = name;
       this.fileIconId = fileIconId;
       this.supportedExtensions = supportedExtensions;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public int getFileIconId()
+   {
+      return fileIconId;
+   }
+
+   public String[] getSupportedExtensions()
+   {
+      return supportedExtensions;
    }
 }
--- FolderBrowser/src/main/java/com/alvessss/folderbrowser/FolderBrowser.java
+++ FolderBrowser/src/main/java/com/alvessss/folderbrowser/FolderBrowser.java
@@ -1,6 +1,9 @@
 package com.alvessss.folderbrowser;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +11,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,6 +29,11 @@ import java.util.ArrayList;
 @SuppressWarnings("all")
 public class FolderBrowser
 {
+   public static final int DEFAULT_COLUMNS = 6;
+
+   public static final String PERMISSION_READ = Manifest.permission.READ_EXTERNAL_STORAGE;
+   public static final String PERMISSION_WRITE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
    private AppCompatActivity parentActivity;
    private Context parentContext;
 
@@ -44,6 +55,19 @@ public class FolderBrowser
 
    private GridLayoutManager gridLayoutManager;
    private int gridLayoutColumns;
+
+   private boolean _needPermision = true;
+   private ActivityResultLauncher<String> permissionLauncher =
+      parentActivity.registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+         if (isGranted)
+         {
+            _start();
+         }
+         else
+         {
+            _permissionNotGranted();
+         }
+      });
 
    public static void childPaths(String root,
                                  ArrayList<String> pathsArr, String targetMimeType)
@@ -98,20 +122,50 @@ public class FolderBrowser
       return new Builder();
    }
 
-   private FolderBrowser()
+   private FolderBrowser() // PRIVATE CONSTRUCTOR OF BUILDER
    {
       ;
    }
+
+   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   private void _start()
+   {
+      if (!_checkPermission(PERMISSION_READ))
+      {
+         permissionLauncher.launch(PERMISSION_READ);
+         return;
+      }
+
+      if (!_checkPermission(PERMISSION_WRITE))
+      {
+         permissionLauncher.launch(PERMISSION_WRITE);
+         return;
+      }
+   }
+
+   private boolean _checkPermission(String permission)
+   {
+      if (ContextCompat.checkSelfPermission(parentContext, permission) == PackageManager.PERMISSION_DENIED)
+      {
+         return false;
+      }
+
+      return true;
+   }
+
+   private void _permissionNotGranted()
+   {
+      ;
+   }
+
+   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    public static class Builder
    {
       private FolderBrowser privateInstance = new FolderBrowser();
 
       private ArrayList<FileSupport> tempTrackedFiles = new ArrayList<>();
-
-      private boolean checkRecyclerView = false;
-      private boolean checkItem = false;
-      private boolean checkDefIcons = true;
 
       public Builder setContext(@NonNull Context context)
       {
@@ -122,26 +176,13 @@ public class FolderBrowser
 
       public Builder setRecyclerView(int id)
       {
-         return setRecyclerView(id, 6);
+         return setRecyclerView(id, FolderBrowser.DEFAULT_COLUMNS);
       }
 
       public Builder setRecyclerView(int id, int columns)
       {
          privateInstance.recyclerViewID = id;
-         privateInstance.recyclerView = privateInstance.parentActivity
-            .findViewById(privateInstance.recyclerViewID);
-
-         if (privateInstance.recyclerView == null)
-         {
-            new RuntimeException("Recycler view not found");
-         }
-
-         privateInstance.gridLayoutManager = new GridLayoutManager(
-            privateInstance.parentActivity,
-            privateInstance.gridLayoutColumns = columns
-         );
-
-         checkRecyclerView = true;
+         privateInstance.gridLayoutColumns = 6;
          return this;
       }
 
@@ -151,7 +192,6 @@ public class FolderBrowser
          privateInstance.itemFilenameID = fileNameId;
          privateInstance.itemFileIconID = fileIconId;
 
-         checkItem = true;
          return this;
       }
 
@@ -160,7 +200,6 @@ public class FolderBrowser
          privateInstance.defaultFileIcon = fileIcon;
          privateInstance.defaultDirectoryIcon = directoryIcon;
 
-         checkDefIcons = true;
          return this;
       }
 
@@ -170,23 +209,127 @@ public class FolderBrowser
          return this;
       }
 
+      public Builder needPermission(boolean value)
+      {
+         privateInstance._needPermision = true;
+         return this;
+      }
+
       public FolderBrowser build()
       {
-         if (checkRecyclerView && checkItem && checkDefIcons)
+         if (_setup())
          {
-            privateInstance.trackedFiles = tempTrackedFiles.toArray(
-               new FileSupport[tempTrackedFiles.size()]);
-
             return privateInstance;
          }
          else
          {
-            new RuntimeException("Missing settings. See the docs > https://google.com");
+            new RuntimeException("Missing some settings. Check the log above!");
             return null;
          }
       }
 
-      private Builder(){};
+      private Builder() // PRIVATE CONSTRUCTOR OF BUILDER
+      {
+         ;
+      }
+
+      private boolean _setup()
+      {
+         if ( _checkContext() &&
+            _checkRecylerViewAndGridLayout() &&
+            _checkRecyclerViewItem() &&
+            _checkDefaultIcons() )
+         {
+            _setTempTrackedFiles();
+            return true;
+         }
+
+         return false;
+      }
+
+      private void _setTempTrackedFiles()
+      {
+         privateInstance.trackedFiles = tempTrackedFiles.toArray(new FileSupport[tempTrackedFiles.size()]);
+      }
+
+      private boolean _checkContext()
+      {
+         if (privateInstance.parentActivity == null ||
+            privateInstance.parentContext == null)
+         {
+            return false;
+         }
+         return true;
+      }
+
+      private boolean _checkRecylerViewAndGridLayout()
+      {
+         privateInstance.recyclerView = privateInstance.parentActivity
+            .findViewById(privateInstance.recyclerViewID);
+
+         privateInstance.gridLayoutManager = new GridLayoutManager(
+            privateInstance.parentContext, (privateInstance.gridLayoutColumns)
+         );
+
+         if (privateInstance.recyclerView == null ||
+            privateInstance.gridLayoutManager == null)
+         {
+            return false;
+         }
+
+         return true;
+      }
+
+      private boolean _checkRecyclerViewItem()
+      {
+         if ( _checkId(privateInstance.itemLayoutID) &&
+            _checkId(privateInstance.itemFilenameID) &&
+            _checkId(privateInstance.itemFileIconID) )
+         {
+            return true;
+         }
+
+         return false;
+      }
+
+      private boolean _checkDefaultIcons()
+      {
+         if (_checkId(privateInstance.defaultFileIcon)
+            && _checkId(privateInstance.defaultDirectoryIcon))
+         {
+            return true;
+         }
+         return false;
+      }
+
+      private boolean _checkId(int id)
+      {
+         if (id != 0)
+         {
+            return true;
+         }
+
+         return false;
+      }
+
+      // unused
+      private boolean _checkLayout(Context context, int layoutId)
+      {
+
+         final View testView;
+         try
+         {
+            testView = LayoutInflater.from(context)
+               .inflate(layoutId, null);
+            return true;
+         }
+         catch (Resources.NotFoundException e)
+         {
+            e.printStackTrace();
+            return false;
+         }
+      }
+
    }
 
    private class _ItemFields
