b77a64e218f3ec1b4814d473b07ae9ba22cf9efd
Alvessss <backtobluepill@gmail.com>
2022-05-27 02:08:11 -0300

    created File and Directory

---
+++ FolderBrowser/src/test/java/com/alvessss/folderbrowser/InodeTest.java
@@ -0,0 +1,12 @@
+package com.alvessss.folderbrowser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+@SuppressWarnings("all")
+public class InodeTest {
+   ;
+}
